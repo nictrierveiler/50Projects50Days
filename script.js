d3c4da95ad38776149f34c4d9c5ac130d9765b8f
Nicolas Trierveiler <nicolastrierveiler@gmail.com>
2022-07-13 02:45:29 -0300

    DONE

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const panels = document.querySelectorAll('.panel')
 
-panels.forEach((panel) => {
+panels.forEach(panel => {
     panel.addEventListener('click', () => {
-       panel.classList.add('active') 
+        removeActiveClasses()
+        panel.classList.add('active')
     })
 })
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+} 
